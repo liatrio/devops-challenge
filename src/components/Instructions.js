9eb73793be418b014a037193cee8e48754c79d3f
shanemacbride <shanemacb@gmail.com>
2018-01-04 17:31:13 -0800

    add instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -10,8 +10,14 @@ class Instructions extends Component {
       <div className="column column-80">
         <h4>Instructions</h4>
         <ol>
-          <li>Fork repo</li>
+          <li>Create a GitHub Account</li>
+          <li>Enter your GitHub Username to the right</li>
+          <li>Fork repo https://github.com/liatrio/microservices-demo (Fork icon link here?)</li>
           <li>Enable Travis CI</li>
+          <li>Fix the build error (rename the file correctly..)</li>
+          <li>Register for relevant play-with-docker account</li>
+          <li>On play-with-docker, git clone your fork, and do docker-compose-up to run sock-shop</li>
+          <li>Enter your live sock-shop play-with-docker URL here:</li>
         </ol>
       </div>
     );
