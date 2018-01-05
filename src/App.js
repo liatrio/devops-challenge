1a73578d25d8509c3dd6c2e75bfbe69ba7eccaf4
shanemacbride <shanemacb@gmail.com>
2018-01-05 15:35:10 -0800

    add progress meter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Instructions from './components/Instructions'
 import Scoreboard from './components/Scoreboard'
 import Info from './components/Info'
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 import 'milligram';
 
 class App extends Component {
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Step } from 'semantic-ui-react';
 
 class Scoreboard extends Component {
   constructor() {
@@ -8,10 +9,44 @@ class Scoreboard extends Component {
   render() {
     return (
       <div className="column">
+        <center>
+          <Step.Group ordered>
+            <Step completed>
+              <Step.Content>
+                <Step.Title>GitHub Account</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step active>
+              <Step.Content>
+                <Step.Title>Fork Sock-shop</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step active>
+              <Step.Content>
+                <Step.Title>Add Travis CI Configuration</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step active>
+              <Step.Content>
+                <Step.Title>Enable Travis CI</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step active>
+              <Step.Content>
+                <Step.Title>Correct Build Error</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step active>
+              <Step.Content>
+                <Step.Title>Run Sock-shop</Step.Title>
+              </Step.Content>
+            </Step>
+          </Step.Group>
         {this.props.user === '' ? 
-          <p className="scoreboard">
+          <p>
           Input your GitHub username to track progress.</p> :
-          <p className="scoreboard">Progress of {this.props.user}</p>}
+          <p>Progress of @{this.props.user}</p> }
+        </center>
       </div>
     );
   }
